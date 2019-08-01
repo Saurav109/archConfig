fa3a08fb7e35045ed458c3a57d9bc5d057825820
saurav109 <scientistboy109@gmail.com>
2019-08-01 06:27:35 +0600

    cleaning script folder mass

--- script/perMin.sh
+++
@@ -1,7 +0,0 @@
-#this script will run every min with cron job
-#for config more go : /var/spool/cron/ironman|root
-
-#notification uptime
-#DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus notify-send "Uptime"  "Your time: $(uptime -p)";
-#updateActiveTime
-uptime -p > ~/uptime.txt
